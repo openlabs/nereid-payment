8acc1ea7b3698b98d4c94d5199c95c749378f280
Sharoon Thomas <sharoon.thomas@openlabs.co.in>
2012-02-24 15:05:28 -0500

    Bump to Version 0.2 Stable

--- __tryton__.py
+++ __tryton__.py
@@ -6,7 +6,7 @@
     'name': 'Nereid - Payment Gateway',
     'description': '''API to facilitate multiple payment gateways to integrate
         with nereid''',
-    'version': '2.0.0.1',
+    'version': '2.0.0.2',
     'author': 'Openlabs Technologies & Consulting (P) LTD',
     'email': 'info@openlabs.co.in',
     'website': 'http://www.openlabs.co.in/',
